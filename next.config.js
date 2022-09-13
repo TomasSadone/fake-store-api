e85d5b4c08e441020a121ff7070abe197aae0cdf
TomasSadone <tomisadone27@gmail.com>
2022-09-13 20:55:48 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+const nextConfig = {};
+// nextConfig,
 
 module.exports = {
-  nextConfig,
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["fakestoreapi.com"],
   },
